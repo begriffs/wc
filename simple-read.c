000b32744bc883966a82f354b53f4eec608c1c6c
Joe Nelson <joe@begriffs.com>
2019-10-19 01:00:54 -0500

    whitespace

--- simple-read.c
+++ simple-read.c
@@ -9,29 +9,29 @@
 
 int main(void)
 {
-    unsigned long long chars=0, words=0, lines=0;
-    bool gotspace = true;
-    char *buf = malloc(BUFSZ), c;
-    ssize_t index, length;
+	unsigned long long chars=0, words=0, lines=0;
+	bool gotspace = true;
+	char *buf = malloc(BUFSZ), c;
+	ssize_t index, length;
 
-    while ((length = read(STDIN_FILENO, buf, BUFSZ)) > 0)
+	while ((length = read(STDIN_FILENO, buf, BUFSZ)) > 0)
 	{
-        for (index = 0; index < length; ++index)
+		for (index = 0; index < length; ++index)
 		{
-            chars++;
-            if (isspace(c = buf[index]))
-            {
-                gotspace = true;
-                if (c == '\n')
-                    lines++;
-            }
-            else if(gotspace)
-            {
-                gotspace = false;
-                words++;
-            }
-        }
-    }
-    free(buf);
-    printf("%llu %llu %llu\n", lines, words, chars);
+			chars++;
+			if (isspace(c = buf[index]))
+			{
+				gotspace = true;
+				if (c == '\n')
+					lines++;
+			}
+			else if(gotspace)
+			{
+				gotspace = false;
+				words++;
+			}
+		}
+	}
+	free(buf);
+	printf("%llu %llu %llu\n", lines, words, chars);
 }
