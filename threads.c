4c5487bc1aac29da6dca903cce41356cec791557
Joe Nelson <joe@begriffs.com>
2019-10-19 00:55:34 -0500

    Use read() rather than fgetc() in threads
    Goes way faster

--- threads.c
+++ threads.c
@@ -1,56 +1,76 @@
 #include <ctype.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <pthread.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 struct counts
 {
 	long chars;
 	long words;
 	long lines;
+
+	char cfirst;
+	char clast;
 };
 
 struct chunk
 {
-	FILE *f;
-	long offset;
-	long length;
+	int fd;
+	off_t offset;
+	size_t length;
 };
+
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define BUFSZ 1024*1024
 
 struct counts *count_chunk(struct chunk *chunk)
 {
+	long chars=0, words=0, lines=0;
 	struct counts *ret = calloc(1, sizeof(struct counts));
-	int c='\0', prev;
+	bool gotspace = true;
+	char c = '\0', *buf = malloc(BUFSZ);
+	ssize_t index, n, remaining = chunk->length;
 
-	fseek(chunk->f, chunk->offset, SEEK_SET);
-	for (prev=' ';
-		 ret->chars < chunk->length && (c = fgetc(chunk->f)) != EOF;
-		 prev=c)
+	lseek(chunk->fd, chunk->offset, SEEK_SET);
+	while ((n = read(chunk->fd, buf, MIN(BUFSZ, remaining))) > 0)
 	{
-		ret->chars++;
-		if (isspace(prev) && !isspace(c))
-			ret->words++;
-		if (c == '\n')
-			ret->lines++;
+		if (!ret->cfirst)
+			ret->cfirst = buf[0];
+		for (index = 0; index < n; ++index)
+		{
+			chars++;
+			if (isspace(c = buf[index]))
+			{
+				gotspace = true;
+				if (c == '\n')
+					lines++;
+			}
+			else if(gotspace)
+			{
+				gotspace = false;
+				words++;
+			}
+		}
+		remaining -= n;
 	}
+	free(buf);
 
-	/* peek ahead and if we stopped mid-word, don't count the
-	 * word because another thread will count the end of it */
-	if (!isspace(c))
-	   if ((c = fgetc(chunk->f)) != EOF && !isspace(c))
-		ret->words--;
-
+	ret->chars = chars;
+	ret->words = words;
+	ret->lines = lines;
+	ret->clast = c;
 	return ret;
 }
 
 int main(int argc, const char **argv)
 {
-	long filesz;
+	off_t filesz;
 	int i, nWorkers = 4;
 	pthread_t *workers;
 	struct chunk *chunks;
@@ -74,18 +94,18 @@ int main(int argc, const char **argv)
 	}
 
 	stat(path, &in_stat);
-	filesz = (long)in_stat.st_size;
+	filesz = in_stat.st_size;
 
 	workers = malloc(nWorkers * sizeof(pthread_t));
 	chunks = malloc(nWorkers * sizeof(struct chunk));
 	for (i = 0; i < nWorkers; i++)
 	{
-		chunks[i].f = fopen(path, "r");
+		chunks[i].fd = open(path, O_RDONLY);
 		chunks[i].offset = i*(filesz/nWorkers);
 		/* due to division rounding, have the last thread read to EOF */
 		chunks[i].length = (i < nWorkers-1) ? filesz/nWorkers : LONG_MAX;
 		pthread_create(&workers[i], NULL,
-			(void *(*)(void *))count_chunk, &chunks[i]);
+				(void *(*)(void *))count_chunk, &chunks[i]);
 	}
 
 	/* run free, threads! */
@@ -93,10 +113,15 @@ int main(int argc, const char **argv)
 	for (i = 0; i < nWorkers; i++)
 	{
 		pthread_join(workers[i], (void **)&result);
-		fclose(chunks[i].f);
+		close(chunks[i].fd);
 		sum.lines += result->lines;
 		sum.words += result->words;
 		sum.chars += result->chars;
+		/* don't double-count a word split between chunks */
+		if (sum.clast && !isspace(sum.clast) && !isspace(result->cfirst))
+			sum.words--;
+		sum.cfirst = result->cfirst;
+		sum.clast = result->clast;
 		free(result);
 	}
 
